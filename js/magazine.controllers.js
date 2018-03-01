21b313a079dfab9b0450518a1fc52c282772ce4c
Suarsan <javiersuarsan@gmail.com>
2018-03-01 13:38:36 +0100

    Implementando lista de contenido recomendado

--- js/magazine.controllers.js
+++ js/magazine.controllers.js
@@ -5,7 +5,7 @@ function getMagazines(){
         type: 'GET',
         dataType: 'json'})
         .then(function(res){
-            console.log(res);
+            // console.log(res);
         },
         function(jqXHR, textStatus, errorThrown) {
             console.log('<p class="error">ERROR: ' + jqXHR.status + ': ' + jqXHR.statusText + '</p>'); });  }
@@ -18,20 +18,35 @@ function getMagazine(id){
         type: 'GET',
         dataType: 'json'})
         .then(function(res){
-            console.log(res);
+            // console.log(res);
             return res;
         },
         function(jqXHR, textStatus, errorThrown) {
             console.log('<p class="error">ERROR: ' + jqXHR.status + ': ' + jqXHR.statusText + '</p>'); }); }
 
 function printMagazineInfo(id, attribute, selector){
-    $.ajax({
-        url: 'http://10.2.201.158:43210/magazines?id='+ id,
-        data: null,
-        type: 'GET',
-        dataType: 'json'})
-        .then(function(res){
-            $(selector).html(res[0][attribute]);
-        },
-        function(jqXHR, textStatus, errorThrown) {
-            console.log('<p class="error">ERROR: ' + jqXHR.status + ': ' + jqXHR.statusText + '</p>'); }); }
+    if(attribute==='thumbnail'){
+        $.ajax({
+            // barra + id
+            url: 'http://10.2.201.158:43210/magazines?id='+ id,
+            data: null,
+            type: 'GET',
+            dataType: 'json'})
+            .then(function(res){
+                console.log('http://10.2.201.158:43210/' + res[0][attribute]);
+                $(selector).css('background-image', 'url(http://10.2.201.158:43210/' + res[0][attribute] + ')');
+            },
+            function(jqXHR, textStatus, errorThrown) {
+                console.log('<p class="error">ERROR: ' + jqXHR.status + ': ' + jqXHR.statusText + '</p>'); });
+    }else{
+        $.ajax({
+            url: 'http://10.2.201.158:43210/magazines?id='+ id,
+            data: null,
+            type: 'GET',
+            dataType: 'json'})
+            .then(function(res){
+                $(selector).html(res[0][attribute]);
+            },
+            function(jqXHR, textStatus, errorThrown) {
+                console.log('<p class="error">ERROR: ' + jqXHR.status + ': ' + jqXHR.statusText + '</p>'); }); }
+} 

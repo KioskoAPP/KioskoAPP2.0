bd70cd07db6880d746a941d83d13fb298facfe8b
Azahara <35915786+azaharabm@users.noreply.github.com>
2018-03-05 12:57:51 +0100

    footer añadido a adjust, mis direcciones y premium. Index comenzado

--- js/abm.js
+++ js/abm.js
@@ -1,3 +1,13 @@
+// Botón de "Login" en Index //
+function goToLogIn(){
+    location.href='/login.html';
+}
+
+// Botón de "Regístrate" en Index//
+function goToRegister(){
+    location.href='/register.html';
+}
+
 // Botón de "Pásate a premium" //
 function goToPrem(){
     location.href='/premium.html';
@@ -18,89 +28,11 @@ function goToCreditCards(){
     location.href='/mistarjetas.html'
 }
 
-// Función para añadir las direcciones del usuario
-function newAddress(){
-    var newUserAdd = $ ('form');
-    var dir ={};
-    newUserAdd.forEach(function (item) {
-        dir[item.Address] = item.value;        
-    });
-    $.ajax({
-        url: 'http://localhost:43210/usuarios',
-        method: 'POST',
-        dataType: 'json',
-        data: dir        
-    }).then(
-        function(){
-            $('#abm_newAdressBtn').off('click', newAddress);
-            volver();
-        },
-        function(jqXHR, textStatus, errorThrown) {
-        $('errorMsg').html(
-            '<p class="error">ERROR: ' + jqXHR.status + ': ' + jqXHR.statusText + '</p>');
-        }
-    );
-
-}
-
-
-function setUserAddresses(id){
-    $.ajax({
-        url: 'http://localhost:43210/usuarios?_id=' + id,
-        type: 'POST',
-        dataType: 'json',
-    })
-        .then(
-            function(res){
-            console.log("aa" + res);
-        },
-        function(jqXHR, textStatus, errorThrown) {
-            console.log('<p> class="error">ERROR: ' + jqXHR.status + ': ' + jqXHR.statusText + '</p>'); 
-        });  
-    }
-
-// Función para mostrar las direcciones del usuario
-/*function seeUserAddresss(verDir){
-    $.ajax({
-        url: 'http://localhost:43210/usuarios/' + verDir,
-        dataType: 'json',
-        //console.log($("#Address").val())
-    }).then(
-        function (resp){
-            var userAddress = '<h4>';
-            userAddress += Address + '</h4>' + '<br>' + '<p>' + streetName + ', '+ number + '.' + '<br>' + postCodeName + ' ' + cityName + ' (' + countryName + ').';
-            userAddress +='</p>';
-        },
-        //console.log(userAddress),
-        )   
-    $('#abm_myAdressDir1').html(userAddress);
-}*/
-
-function seeUserAddresss (){
-    $.get('http://localhost:43210/usuarios/', function (address){
-        var rslt = $('<h4>' + Address + '</h4>' + '<br>' + '<p>' + streetName + ', '+ number + '.' + '<br>' + postCodeName + ' ' + cityName + ' (' + countryName + ').');
-        userAddress +='</p>';
-        $('#list').empty().append(rslt);
-        for (var i=0; i<address.length; i++){
-           var tr = $('<h4>');
-           tr.append($(address[i].Address + '</h4>'));
-           tr.append($('<p>' + address[i].streetName + ', </p>'));
-           tr.append($('<p>' + address[i].number + '.</p><br>'));
-           tr.append($('<p>' + address[i].postCodeName + ' </p>'));
-           tr.append($('<p>' + address[i].cityName + ' </p>'));
-           tr.append($('<p>(' + address[i].countryName + ').</p>'));
-        }
-    }, 'json');
-}
-
-
-
 $(document).ready(function() {
+    $('#abm_loginBtn').click(goToLogIn);
+    $('#abm_registerBtn').click(goToRegister);
     $('#abm_premBtn').click(goToPrem);
     $('#abm_bePremBtn').click(goToBePrem);
     $('#abm_myAdress').click(goToDir);
     $('#abm_myCreditCards').click(goToCreditCards);
-    $('#abm_newAdress').click(seeUserAddresss);
-    $('#abm_newAdressBtn').on('click', newAddress);
-    setUserAddresses(id);
 });

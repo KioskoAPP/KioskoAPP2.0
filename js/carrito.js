72bf9f65a2b9c055b7b480aedd7577c76f55bc99
Azahara <35915786+azaharabm@users.noreply.github.com>
2018-03-14 12:58:20 +0100

    carrito sin terminar

--- js/carrito.js
+++ js/carrito.js
@@ -1,7 +1,8 @@
-function LineaPedido(idProducto, name) {
+function LineaPedido(idProducto, title, owner) {
     this.id = 0;
     this.idProducto = idProducto;
-    this.name = name;
+    this.title = title;
+    this.owner = owner;
 }
 
 /** Elementos en el carrito **/
@@ -15,6 +16,21 @@ var carrito = new (function () {
         if (localStorage)
             localStorage['CarritoCompra'] = JSON.stringify(obj.lineas);
     }
+
+
+    /***** Función para añadir productos al carrito *******/
+    obj.add = function (title, owner) {
+        var ln = new LineaPedido(title, owner);
+        if (obj.lineas.length == 0) {
+            ln.id = 1;
+        } else {
+            var old = obj.lineas.find(function (item) { return item.idProducto == idProducto; });
+            ln.id = obj.lineas[obj.lineas.length - 1].id + 1;
+        }
+        obj.lineas.push(ln);
+        guardaCarrito();
+    };
+
 
     /** Función para eliminar UNA LÍNEA **/
     obj.removeLine = function (id) {
@@ -40,15 +56,29 @@ var carrito = new (function () {
 
 
 function productosEnCarrito(id) {
-    var tmpl = $('#tmplListadoCarrito').html();
-    var rslt = Mustache.render(tmpl, { filas: carrito.lineas });
-    $('#abm_carrito').html(rslt);
 
     var obj = this;
     var listaProductos;
+
+    obj.Refresca = function () {
+        var tmpl = $('#tmplListadoCarrito').html();
+        var rslt = Mustache.render(tmpl, { filas: carrito.lineas });
+        $('#abm_carrito').html(rslt);
+        //$('#abm_carrito').append(rslt);
+    };
+    function formateaProducto(prod) {
+        return "<p>"+prod+"</p>";
+    }
+    obj.pintaProducto = function () {
+        $('#abm_productos').empty();
+        for (i = 0; i < listaProductos.length; i++) {
+            $('#abm_productos').append(formateaProducto(listaProductos[i]));
+        }
+
+    };
     obj.mostrarProductos = function () {
         if (listaProductos) {
-            productosEnCarrito(listaProductos);
+            obj.pintaProducto();
         } else
             $.ajax({
                 url: 'http://localhost:43210/usuarios/' + id,
@@ -56,12 +86,12 @@ function productosEnCarrito(id) {
             }).then(
                 function (resp) {
                     listaProductos = resp.cart;
-                    console.log(listaProductos);
-                    productosEnCarrito(listaProductos);
+                    obj.pintaProducto();
                 },
                 function (jqXHR, textStatus, errorThrown) {
-                    //reject(jqXHR, textStatus, errorThrown);
+                    reject(jqXHR, textStatus, errorThrown);
                 }
             );
     };
+
 }
